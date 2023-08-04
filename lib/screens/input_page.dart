8d9a3e74c6b1828bd956ea2dad6625d6c775742c
Mohammad Murtada <118128637+MYMurtada@users.noreply.github.com>
2023-08-04 11:21:12 +0300

    Update input_page.dart

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:bmi_calculator/screens/resultsPage.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
